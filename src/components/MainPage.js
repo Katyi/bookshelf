fa9e3d35f6b4d157d9ae3ec897bd218f6fd0529d
Alexandra Egorova <egorova.aleksandra@gmail.com>
2023-01-13 08:59:56 +0300

    changes in MainPage

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -4,6 +4,7 @@ import React from 'react';
 import shelfBackground from '../shelfs/shelfBackground.jpg';
 import shelf from '../shelfs/shelf.png';
 import book1 from '../books/Moler/book1.png';
+import book2 from '../books/Moler/book2.png';
 import book3 from '../books/LopeDeVega/book3.png';
 
 const styles = {
@@ -11,24 +12,25 @@ const styles = {
     flexGrow: 1,
     flex: 1,
     padding: 0.1,
-    backgroundImage: `url(${shelfBackground})`, 
+    backgroundImage: `url(${shelfBackground})`,
     backgroundRepeat: "no-repeat",
     backgroundPosition: "center center",
-    backgroundSize: "cover"
+    backgroundSize: "cover",
   },
 };
 
 function MainPage() {
   return (
     <Paper style={styles.paperContainer}>
-      <Stack direction={'column'} height={'100vh'}>
-        <Stack direction={'row'} gap={0} height={'20vh'} mb={-10} pl={70} zIndex={2} >
-          <Box component='img' src={book1} height={290} mt={7}></Box>
-          <Box component='img' src={book3} height={300} mt={6.7}></Box>
+      <Stack direction={'column'} height={'100vh'} width={'100vw'}>
+        <Stack direction={'row'} gap={0} height={'26vh'} mt={'15vh'} mb={'-35vh'} ml={'20vw'}>
+          <Box component='img' src={book1} height={'100%'} ></Box>
+          <Box component='img' src={book3} height={'100%'} ></Box>
+          <Box component='img' src={book2} height={'100%'} ></Box>
         </Stack>
-        <Box component='img' src={shelf} zIndex={0} pb={0} height={'35vh'}>
+        <Box component='img' src={shelf} zIndex={0} pb={0} height={'45vh'}>
         </Box>
-        <Box component='img' src={shelf} zIndex={0} pb={0} height={'35vh'}></Box>
+        <Box component='img' src={shelf} zIndex={0} pb={0} height={'45vh'}></Box>
       </Stack>
     </Paper>
   )
